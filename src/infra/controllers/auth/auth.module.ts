0126fd50189d017b810d5aa077f398f7767af08d
Kevin <bastokevindaniel@gmail.com>
2021-09-30 00:19:01 -0500

    verify module, controller, and service created

--- src/infra/controllers/auth/auth.module.ts
+++ src/infra/controllers/auth/auth.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { LoginModule } from './login/login.module';
 import { RegisterModule } from './register/register.module';
+import { VerifyModule } from './verify/verify.module';
 
 @Module({
-  imports: [LoginModule, RegisterModule]
+  imports: [LoginModule, RegisterModule, VerifyModule]
 })
 export class AuthModule {}
