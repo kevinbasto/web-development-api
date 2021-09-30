030d1aa9736c9c5971fd44c100b6f6ea097118c3
Kevin <bastokevindaniel@gmail.com>
2021-09-29 23:40:37 -0500

    added repos module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ControllersModule } from './infra/controllers/controllers.module';
 import { ServicesModule } from './infra/services/services.module';
+import { ReposModule } from './infra/repos/repos.module';
 
 @Module({
-  imports: [ControllersModule, ServicesModule],
+  imports: [ControllersModule, ServicesModule, ReposModule],
   controllers: [],
   providers: [],
 })
