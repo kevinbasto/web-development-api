145322946be831aabcdaeb7627a513826971c915
Kevin <bastokevindaniel@gmail.com>
2021-09-29 23:40:41 -0500

    added guards module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ControllersModule } from './infra/controllers/controllers.module';
 import { ServicesModule } from './infra/services/services.module';
 import { ReposModule } from './infra/repos/repos.module';
+import { GuardsModule } from './infra/guards/guards.module';
 
 @Module({
-  imports: [ControllersModule, ServicesModule, ReposModule],
+  imports: [ControllersModule, ServicesModule, ReposModule, GuardsModule],
   controllers: [],
   providers: [],
 })
