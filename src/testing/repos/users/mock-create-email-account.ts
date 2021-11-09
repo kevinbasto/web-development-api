cbb43b187f7dbd038ef6e70e7c021e085f65ae40
Kevin <bastokevindaniel@gmail.com>
2021-11-09 10:45:46 -0600

    correction for the mockcreator

--- src/testing/repos/users/mock-create-email-account.ts
+++ src/testing/repos/users/mock-create-email-account.ts
@@ -1,7 +1,7 @@
 import { User } from "../../../core/instances/auth/user";
 import { CreateUserRepo } from "../../../core/repos/users/create-user.interface";
 
-export class MockCreateEmailAccount implements CreateUserRepo{
+export class MockCreateUser implements CreateUserRepo{
     createUser(lang : string, user : User, accountId : string) : Promise<void>{
         return;
     }
