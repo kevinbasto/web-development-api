98b3c15b1424750631369e1103062c475d86a35d
Kevin <bastokevindaniel@gmail.com>
2021-11-08 23:01:35 -0600

    cleaning up the structure and big remake for many services

--- src/core/app/auth/login/email.login.spec.ts
+++ src/core/app/auth/login/email.login.spec.ts
@@ -1,40 +1,8 @@
-import { PasswordCypher } from '../../../ports/password-cypher.interface';
-import { SessionHandler } from '../../../ports/session-handler.interface';
-import { Translater } from '../../../ports/translater.interface';
-import { AccountsRepo } from '../../../repos/accounts.repo.interface';
 import { EmailLogin } from './email.login';
-
-class mockaccountsRepo implements AccountsRepo{
-  async createEmailAccount(email : string, password : string) {}
-  async getAccountByEmail(email : string) {}
-}
-
-class mocktranslater implements Translater{
-  async getTranslation(lang : string, message : string, params? : any) : Promise<string> {
-    return "";
-  }
-}
-
-class mockSessionHandler implements SessionHandler{
-  decodeToken
-  signSession
-  verifySession
-  verifyrefreshToken
-}
-
-class mockPasswordCypher implements PasswordCypher{
-  signPassword
-  verifyPassword
-}
-
-const mockrepo = new mockaccountsRepo();
-const sessionHandler  = new mockSessionHandler();
-const passwordCypher = new mockPasswordCypher();
-const mocktranslate = new mocktranslater();
 
 
 describe('Login', () => {
   it('should be defined', () => {
-    expect(new EmailLogin(mockrepo,  passwordCypher, sessionHandler, mocktranslate)).toBeDefined();
+    expect(new EmailLogin()).toBeDefined();
   });
 });
---
+++ src/core/app/messages/email-send/email-send.spec.ts
@@ -0,0 +1,7 @@
+import { EmailSend } from './email-send';
+
+describe('EmailSend', () => {
+  it('should be defined', () => {
+    expect(new EmailSend()).toBeDefined();
+  });
+});
