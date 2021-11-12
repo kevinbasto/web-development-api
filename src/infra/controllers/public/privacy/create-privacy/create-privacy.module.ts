3462adc47ebddc3d6f89bcd375058a9b1af14cf9
Kevin <bastokevindaniel@gmail.com>
2021-11-11 21:53:04 -0600

    create privacy service created

--- src/infra/controllers/public/privacy/create-privacy/create-privacy.module.ts
+++ src/infra/controllers/public/privacy/create-privacy/create-privacy.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CreatePrivacyController } from './create-privacy.controller';
+import { CreatePrivacyService } from './create-privacy.service';
 
 @Module({
-  controllers: [CreatePrivacyController]
+  controllers: [CreatePrivacyController],
+  providers: [CreatePrivacyService]
 })
 export class CreatePrivacyModule {}
--- src/infra/controllers/public/privacy/privacy.module.ts
+++ src/infra/controllers/public/privacy/privacy.module.ts
@@ -4,6 +4,7 @@ import { EditPrivacyModule } from './edit-privacy/edit-privacy.module';
 import { GetPrivacyModule } from './get-privacy/get-privacy.module';
 import { DeletePrivacyModule } from './delete-privacy/delete-privacy.module';
 
+
 @Module({
   imports: [CreatePrivacyModule, EditPrivacyModule, GetPrivacyModule, DeletePrivacyModule],
 })
