861646f2f72198737ffb25ac2a0ecdc4b1d7c2f4
Kevin <bastokevindaniel@gmail.com>
2021-11-12 06:28:54 -0600

    create privacy spec files will now compile

--- src/core/app/public/create-privacy/create-privacy.spec.ts
+++ src/core/app/public/create-privacy/create-privacy.spec.ts
@@ -1,7 +1,14 @@
+import { UuidGeneratorService } from '../../../../infra/tools/uuid-generator/uuid-generator.service';
+import { MockCreatePrivacy } from '../../../../testing/repos/public/privacy/mock-create-privacy';
+import { MockTranslater } from '../../../../testing/tools/mock-translater';
 import { CreatePrivacy } from './create-privacy';
+
+const uuidGenerator = new UuidGeneratorService();
+const createPrivacyRepo = new MockCreatePrivacy();
+const translater = new MockTranslater();
 
 describe('CreatePrivacy', () => {
   it('should be defined', () => {
-    expect(new CreatePrivacy()).toBeDefined();
+    expect(new CreatePrivacy(uuidGenerator, createPrivacyRepo, translater)).toBeDefined();
   });
 });
