c48ea80f6e02259ee14b2a57ef0aab13d1d39471
Kevin <bastokevindaniel@gmail.com>
2021-11-14 22:09:54 -0600

    edit privacy repos updated

--- src/infra/controllers/public/privacy/edit-privacy/edit-privacy.module.ts
+++ src/infra/controllers/public/privacy/edit-privacy/edit-privacy.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ReposModule } from '../../../../repos/repos.module';
+import { ToolsModule } from '../../../../tools/tools.module';
 import { EditPrivacyController } from './edit-privacy.controller';
 import { EditPrivacyService } from './edit-privacy.service';
 
 @Module({
+  imports: [ToolsModule, ReposModule],
   controllers: [EditPrivacyController],
   providers: [EditPrivacyService]
 })
--- src/infra/repos/public/privacy-repos/edit-privacy-repo/edit-privacy-repo.service.spec.ts
+++ src/infra/repos/public/privacy-repos/edit-privacy-repo/edit-privacy-repo.service.spec.ts
@@ -1,4 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrivacyDto } from '../../../../../core/dto/public/privacy';
+import { ToolsModule } from '../../../../tools/tools.module';
 import { EditPrivacyRepoService } from './edit-privacy-repo.service';
 
 describe('EditPrivacyRepoService', () => {
@@ -6,6 +8,7 @@ describe('EditPrivacyRepoService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports : [ToolsModule],
       providers: [EditPrivacyRepoService],
     }).compile();
 
@@ -15,4 +18,23 @@ describe('EditPrivacyRepoService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should edit a post successfully', async () => {
+    try {
+      let privacy : PrivacyDto = {
+        title : "actualización del aviso de privacidad",
+        date : "dec 1, 2021",
+        content: [
+          "we came a romans",
+          "no mercy",
+          "testing for the testy test"
+        ]
+      }
+      let privacyId : string = "373ad2fea10dde9c2ec5521db4d7093820a26d1dda119735a39932b639121cf4be794e00da389ff6ed8b304f80060252d762f0454efa666653b5c0c0b0807384";
+      await service.updatePrivacyTerms("en", privacyId, privacy)
+    } catch (error) {
+      expect(error).toBeUndefined();
+    }
+  })
+
 });
